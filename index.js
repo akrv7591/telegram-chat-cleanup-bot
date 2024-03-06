76cfc83676444786044c9cc317db186107440461
a7591 <a7591@zeroweb.kr>
2024-03-06 09:42:38 +0900

    initial comit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// Replace 'YOUR_BOT_TOKEN' with the token you obtained from BotFather
+const TOKEN = process.env['TELEGRAM_BOT_TOKEN']
+
+if (!TOKEN) {
+  console.error('No token provided');
+  process.exit(1);
+}
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', (msg) => {
+    if (msg.new_chat_members || msg.left_chat_member) {
+        // Delete "user joined" and "user left" messages
+        console.log(`TRYING TO DELETE ${msg}`)
+        bot.deleteMessage(msg.chat.id, msg.message_id)
+          .catch(e => console.error(e))
+          .then(value => console.log(value))
+    }
+});
+
+// Handle /start command
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Bot is running!')
+      .catch(e => console.error(e))
+      .then(value => console.log(value));
+});
